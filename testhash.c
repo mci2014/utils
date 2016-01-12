bc79385c55fd619b582dbe92087025f6301ed91e
Alan Wang <alan.wang@csr.com>
2016-01-12 13:41:57 +0800

    Temp work of testhash unit

---
+++ testhash.c
@@ -0,0 +1,23 @@
+/*****************************************************************************
+*
+* @File       testhash.c
+*
+* @Author     Alan Wang
+*
+* @Purpose    Test unit for Hash
+*
+* The MIT License (MIT)
+* https://opensource.org/licenses/MIT
+*
+*****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <hash.h>
+
+int main() {
+
+    return 0;
+}
+
+
